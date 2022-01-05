e2d0c646a10edfbd7c8e683dc7c1e1cf1b58fe59
corteisjr <corteisjunior@gmail.com>
2022-01-05 20:53:24 +0200

    new commit

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,35 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
-
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name = 'posts')
+    status = models.CharField(max_length=10, choices =STATUS_CHOICES, default='draft')
+    
+    class Meta:
+        ordering = ('-publish',)
+        
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
@@ -19,4 +37,5 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    
     
